dc1d5a5997644b0c8713de51ed87fa2d1e2a7a65
Mostefaoui <m.mostefaoui@hotmail.fr>
2018-07-17 15:52:04 +0100

    Canary deployment

---
+++ src/Spike.Ambassador.Api.V2/ApplicationSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace Spike.Ambassador.Api.V2
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+
+    public static class ApplicationSettings
+    {
+        public static string ApiBaseUrl { get; }
+        public static string InternalApiBaseUrl { get; }
+        
+        static ApplicationSettings()
+        {
+            var settingsResolver = GetSettingsResolver();
+            ApiBaseUrl = settingsResolver("Spike.Ambassador.Api.BaseUrl");
+            InternalApiBaseUrl = settingsResolver("Spike.Ambassador.Internal.Api.BaseUrl");
+        }
+
+        private static Func<string, string> GetSettingsResolver()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.json");
+            var configuration = configurationBuilder.Build();
+
+            return (name) => configuration.GetSection(name).Value;
+        }
+    }
+}
---
+++ src/Spike.Ambassador.Api.V2/Helpers/SpikeAmbassadorHttpClient.cs
@@ -0,0 +1,23 @@
+﻿namespace Spike.Ambassador.Api.V2.Helpers
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+
+    public static class SpikeAmbassadorHttpClient
+    {
+        public static HttpClient GetClient()
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(ApplicationSettings.InternalApiBaseUrl)
+            };
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
